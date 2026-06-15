2ec74e35ac10046791b09909e0f940826e601c7b
Jeffrey <Jefferymsd101@gmail.com>
2026-06-15 23:07:57 +0530

    Update server port log

--- server.js
+++ server.js
@@ -37,5 +37,5 @@ app.put("/api/expenses/:id", async (req, res) => {
 });
 
 app.listen(process.env.PORT, () => {
-  console.log("Server running on port 3000");
+  console.log(`Server running on port ${process.env.PORT}`);
 });
