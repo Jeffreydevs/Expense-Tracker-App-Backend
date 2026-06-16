f567bc4bfeb51259e4fea76bb869b2f6c7112751
Jeffrey <Jefferymsd101@gmail.com>
2026-06-16 14:01:15 +0530

    feat: protect expense routes with JWT middleware

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ require("./db");
 const authRoutes = require("./routes/authRoutes");
 const express = require("express");
 const Expense = require("./models/Expense");
-const cors = require("cors")
+const cors = require("cors");
+const authMiddleware = require("./middleware/authMiddleware");
 
 
 const app = express();
@@ -15,26 +16,26 @@ app.get("/", (req, res) => {
   res.send("Hello Jeffrey");
 });
 
-app.get("/api/expenses", async (req, res) => {
+app.get("/api/expenses", authMiddleware, async (req, res) => {
   const expenses = await Expense.find();  
   res.json(expenses);
 });
 
-app.post("/api/expenses", async (req, res) => {
+app.post("/api/expenses", authMiddleware, async (req, res) => {
   console.log("POST route hit");
   console.log(req.body);  
   const expense = await Expense.create(req.body);
   res.json(expense);
 });
 
-app.delete("/api/expenses/:id", async (req, res) => {
+app.delete("/api/expenses/:id", authMiddleware, async (req, res) => {
   await Expense.findByIdAndDelete(req.params.id);
   res.json({
     message: "Expense deleted",
   });
 });
 
-app.put("/api/expenses/:id", async (req, res) => {
+app.put("/api/expenses/:id", authMiddleware, async (req, res) => {
   const updatedExpense = await Expense.findByIdAndUpdate( req.params.id, req.body, { new: true } );
   res.json(updatedExpense);
 });
